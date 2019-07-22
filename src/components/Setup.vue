a724dc7bfee10b2e357f571d853d9456869bfd88
benj <benj@knowbe4.com>
2019-07-22 17:26:04 -0400

    gist thing finally worked

--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -7,29 +7,32 @@
     ol
       li Go into src/components and delete the HelloWorld.vue file
       li Go into src/views and delete the About.vue file
-      li in src/views, update Home.vue file: 
-        a(
-          href='https://gist.github.com/benjaminkent/d59a6e2a9735240887c15bd00101f42e'
-          target='_blank'
-          rel='no referrer noopener'
-        ) View Gist
-      li in src, update router.js file: 
-        a(
-          href='https://gist.github.com/benjaminkent/970cc97910d45298d10df78765f394fc'
-          target='_blank'
-          rel='no referrer noopener'
-        ) View Gist
-      li in src, update App.vue file: 
-        a(
-          href='https://gist.github.com/benjaminkent/d50a5b7bde6b84180418c1db4474d43d'
-          target='_blank'
-          rel='no referrer noopener'
-        ) View Gist
+      li
+        | in src/views, update Home.vue file:
+        .gist(id='homeGist')
+      li
+        | in src, update router.js file:
+        .gist(id='routerGist')
+      li
+        | in src, update App.vue file:
+        .gist(id='appGist')
 </template>
 
 <script>
+import VueEmbedGist from 'vue-embed-gist'
+import postscribe from 'postscribe'
+/* eslint-disable no-useless-escape */
+
 export default {
-  name: 'setup'
+  name: 'setup',
+  components: {
+    VueEmbedGist
+  },
+  mounted () {
+    postscribe('#homeGist', `<script type="application/javascript" src="https://gist.github.com/benjaminkent/d59a6e2a9735240887c15bd00101f42e.js"><\/script>`)
+    postscribe('#routerGist', `<script src="https://gist.github.com/benjaminkent/970cc97910d45298d10df78765f394fc.js"><\/script>`)
+    postscribe('#appGist', `<script src="https://gist.github.com/benjaminkent/d50a5b7bde6b84180418c1db4474d43d.js"><\/script>`)
+  }
 }
 </script>
 
@@ -38,6 +41,10 @@ export default {
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
+}
+
+.gist {
+  margin-top: 5px;
 }
 
 .setup-container {
