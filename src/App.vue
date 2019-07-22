6b63241d4b92d2542a8a81196d9da3a63549b6e5
benj <benj@knowbe4.com>
2019-07-22 12:36:55 -0400

    started

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  .app-container
+    header
+      h1 Let's Build an App Together!
+    router-view
 </template>
 
+
 <style lang="scss">
-#app {
+.app-container {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #222;
+  background-color: #eaeaea;
+  min-height: 100vh;
+  min-width: 100vw;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+header {
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+  h1 {
+    margin: 0;
   }
+}
+
+body {
+  margin: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .home-container
+    h3 home view
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style lang="scss" scoped>
+.home-container {
+  background-color: #fff;
+  width: 90%;
+  border-radius: 5px;
+  margin: 0 auto;
+  box-shadow: 0 0 2px 2px #22222220;
+}
+</style>
